355c41353a8861eafa32ed702cdefce43daf4099
Emma <kottdjur@hotmail.com>
2018-05-16 17:08:06 +0200

    First session

--- Part_2/code/08-reactiveValues.R
+++ Part_2/code/08-reactiveValues.R
@@ -3,6 +3,9 @@
 library(shiny)
 
 ui <- fluidPage(
+  sliderInput(inputId = "num", 
+              label = "Choose a number", 
+              value = 100, min = 1, max = 100),
   actionButton(inputId = "norm", label = "Normal"),
   actionButton(inputId = "unif", label = "Uniform"),
   plotOutput("hist")
@@ -14,6 +17,7 @@ server <- function(input, output) {
 
   observeEvent(input$norm, { rv$data <- rnorm(100) })
   observeEvent(input$unif, { rv$data <- runif(100) })
+  observeEvent(input$num, { rv$data <- rnorm(input$num) })
 
   output$hist <- renderPlot({ 
     hist(rv$data) 
