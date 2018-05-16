b11466b5d1285b30731f6b59470227082de772d7
Emma <kottdjur@hotmail.com>
2018-05-16 15:57:16 +0200

    Initial commit

---
+++ Part_1/code/first_app.R
@@ -0,0 +1,16 @@
+library(shiny)
+
+ui <- fluidPage(
+  sliderInput(inputId = "num", 
+    label = "Choose a number", 
+    min = 1, max = 100, value = 25),
+  plotOutput("hist")
+)
+
+server <- function(input, output) {
+  output$hist <- renderPlot({
+    hist(rnorm(input$num))
+  })
+}
+
+shinyApp(ui = ui, server = server)
